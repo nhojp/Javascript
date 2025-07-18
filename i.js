ecb4d00c59efdd9aeaa04df424fb386d11b46588
nhojp <johnpaulmarmanjac@gmail.com>
2025-07-18 13:57:37 +0800

    new day, new knowledge

---
+++ i.js
@@ -0,0 +1,8 @@
+//Create an array called colors with 3 colors. 
+// Display the first color, the last color, and the total number of colors.
+
+let colors = ["red", "blue", "green"]
+
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors.length);
---
+++ j.js
@@ -0,0 +1,21 @@
+//let numbers = [1, 2, 3, 4, 5];
+
+//for (let i = 0; i < numbers.length; i++) {
+//    console.log(numbers[i]);
+//}
+//This shows each number: 1, 2, 3, 4, 5 (one per line)
+//What's new:
+
+//for loop repeats code multiple times
+//i = 0 starts counting from 0
+//i < numbers.length keeps going until it reaches the end
+//i++ adds 1 to i each time
+
+//Create an array called names with 4 names. Use a for loop to display each name.
+
+
+let names = ["jp", "john", "paul", "paulmar"];
+
+for (let i = 0; i < names.length; i++){
+    console.log(names[i]);
+}
